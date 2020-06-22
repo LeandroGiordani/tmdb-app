f17d5ab5889967028eeaec842b50101dec62cb0b
leandro.giordani <leandro.giordani@psafe.com>
2020-06-22 04:35:55 -0300

    Create Enum for genres ids

---
+++ app/src/main/java/com/tmdb/codechallenge/home/ui/GenresEnum.kt
@@ -0,0 +1,12 @@
+package com.tmdb.codechallenge.home.ui
+
+enum class GenresEnum(val id: Int) {
+    ACTION(28),
+    ANIMATION(16),
+    COMEDY(35),
+    DOCUMENTARY(99),
+    DRAMA(18),
+    HORROR(27),
+    ROMANCE(10749),
+    THRILLER(53)
+}
